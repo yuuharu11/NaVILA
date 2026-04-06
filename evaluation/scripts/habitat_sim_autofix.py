339a3855f41567023de094e02042f42c266ab0b9
Yuya Yamamoto <yuya.yamamoto@co-design.t.u-tokyo.ac.jp>
2026-04-06 09:18:27 +0000

    pathの編集と実行

--- evaluation/scripts/habitat_sim_autofix.py
+++ evaluation/scripts/habitat_sim_autofix.py
@@ -1,23 +1,16 @@
-import site
-import glob
 import os
 import shutil
 
 replacement_file = os.path.join(os.getcwd(), "evaluation", "replace", "common.py")
 
-site_pkgs = site.getsitepackages()
+# 直接パス指定
+file_path = "/work/habitat-sim/habitat_sim/utils/common.py"
 
-habitat_sim_paths = [
-    path for pkg in site_pkgs for path in glob.glob(os.path.join(pkg, 'habitat_sim-0.1.7*.egg', 'habitat_sim', 'utils', 'common.py'))
-]
-
-if habitat_sim_paths:
-    file_path = habitat_sim_paths[0]
-
+if os.path.exists(file_path):
     if os.path.exists(replacement_file):
         shutil.copy2(replacement_file, file_path)
         print(f'Replaced {file_path} with {replacement_file}')
     else:
         print(f'Error: Replacement file {replacement_file} not found!')
 else:
-    print('Error: habitat_sim/utils/common.py not found!')
+    print('Error: habitat-sim/utils/common.py not found!')
